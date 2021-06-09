c64854c6ec600d73e963ca04740c5c7870efc91c
Martin Ho <martin.h@hotmail.ca>
2021-06-09 00:19:50 -0700

    Initial set-up

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+        <p> Welcome to my site</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
